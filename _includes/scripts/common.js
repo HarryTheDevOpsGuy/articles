0a2b857b0eeecad5dc11393b6ee2c18a3a72eb56
Harry <mr.harry41@gmail.com>
2024-11-23 22:38:34 +0530

    printMe

--- _includes/scripts/common.js
+++ _includes/scripts/common.js
@@ -5,10 +5,3 @@
     document.addEventListener('touchstart', function(){}, false);
   }
 })();
-(function printDiv(divName){
-  var printContents = document.getElementById(divName).innerHTML;
-  var originalContents = document.body.innerHTML;
-  document.body.innerHTML = printContents;
-  window.print();
-  document.body.innerHTML = originalContents;
-})();
